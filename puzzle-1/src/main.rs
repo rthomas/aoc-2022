f7a619b09f8a93658fc8ebe060c13f623d97a988
Ryan Thomas <ryan@ryant.org>
2022-12-04 11:10:11 +1100

    Puzzle 1 Part 2 complete.

--- puzzle-1/src/main.rs
+++ puzzle-1/src/main.rs
@@ -9,12 +9,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut max = 0;
     let mut acc = 0;
 
+    let mut totals = Vec::new();
+
     for line in reader.lines() {
         match line?.as_str() {
             "" => {
                 if acc > max {
                     max = acc;
                 }
+                totals.push(acc);
                 acc = 0;
             }
             l => {
@@ -22,7 +25,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     }
-    println!("{max}");
+    println!("Part 1: {max}");
+
+    assert!(totals.len() >= 3);
+    totals.sort_by(|a, b| b.cmp(a));
+    println!("Part 2: {}", totals[0] + totals[1] + totals[2]);
 
     Ok(())
 }
