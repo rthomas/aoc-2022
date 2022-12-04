f21d165f634af4352ccca64b445c68cba679f588
Ryan Thomas <ryan@ryant.org>
2022-12-04 15:51:06 +1100

    Puzzle 2 part 2

--- puzzle-3/src/main.rs
+++ puzzle-3/src/main.rs
@@ -10,8 +10,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut acc = 0;
 
+    let mut p2_acc = 0;
+    let mut p2_lines = Vec::new();
+
     for line in reader.lines() {
         let line = line?;
+        let mut p2_comp = HashSet::with_capacity(line.len());
+        for c in line.chars() {
+            p2_comp.insert(c);
+        }
+        p2_lines.push(p2_comp);
+
         let parts = line.split_at(line.len() / 2);
 
         let mut comp1 = HashSet::with_capacity(line.len() / 2);
@@ -33,6 +42,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     println!("Part 1: {acc}");
+
+    for triple in p2_lines.as_slice().chunks(3) {
+        let i = triple[0]
+            .intersection(&triple[1])
+            .cloned()
+            .collect::<HashSet<char>>();
+        let mut i = i.intersection(&triple[2]);
+
+        let c = i.next().expect("no intersection found");
+        p2_acc += get_priority(c);
+    }
+
+    println!("Part 2: {p2_acc}");
 
     Ok(())
 }
